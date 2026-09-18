6001306096a8bdaa561f85c83be570f073ea88fe
Dirceu Jr. <104853922+Dirceu06@users.noreply.github.com>
2026-09-18 15:56:45 -0300

    mudanças no funcionamento

--- estacionamento.py
+++ estacionamento.py
@@ -4,24 +4,31 @@
 
 class Estacionamento:
     def __init__(self):
-        self.vagas_disponiveis = [0, 1, 2, 3, 4, 5] 
+        self.vagas_disponiveis = [0, 1, 2, 3, 4, 5]
         self.vagas_ocupadas = [6, 7, 8, 9, 10]
         self.vagas_autorizadas = [11, 12, 13]  # Vagas que requerem autorização
 
-    def vagas_disponiveis(self):
-        return (self.vagas_disponiveis + self.vagas_autorizadas + self.vagas_ocupadas)
+    def listar_vagas(self):
+        return self.vagas_disponiveis + self.vagas_ocupadas + self.vagas_autorizadas
 
     def ocupar_vaga(self, vaga, autorizado=False):
         if not autorizado and vaga in self.vagas_autorizadas:
             return False
+
         if vaga in self.vagas_disponiveis:
             self.vagas_disponiveis.remove(vaga)
             self.vagas_ocupadas.append(vaga)
             return True
+
+        if vaga in self.vagas_autorizadas and autorizado:
+            self.vagas_autorizadas.remove(vaga)
+            self.vagas_ocupadas.append(vaga)
+            return True
+
         return False
 
 
 def vagas(estacionamento):
-    return estacionamento.vagas_disponiveis
+    return estacionamento.listar_vagas()
     
     
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 #requisitos a ser implementados
-# - (usuário) deve mostrar quais vagas disponíveis no estacionamento em tempo real
-# - (usuário) deve impedir ocupação de vagas que requerem autorização
+# - (usuário) deve ser simples de usar
+# - (usuário) deve mostrar quais vagas disponíveis no estacionamento em tempo real (15 min)
+# - (usuário) deve impedir ocupação de vagas que requerem autorização (10 min)
+# - IA foi utilizado para implentação da classe e arrumar algumas funções 
 
 import estacionamento
 from estacionamento import vagas
 
 vagas_class = estacionamento.Estacionamento()
 
-print("vagas disponíveis: ", vagas(vagas_class))  # Mostrar vagas disponíveis
-#interação com o usuário para ocupar uma vaga
+print("vagas: ", vagas(vagas_class), "\nVagas ocupadas: ", vagas_class.vagas_ocupadas, "\nvagas autorizadas: ", list(vagas_class.vagas_autorizadas))  # Mostrar vagas disponíveis
 c = input("Digite o número da vaga que deseja ocupar: ")
 c = int(c)
-# tem autorização para ocupar a vaga?
 
-r = input("Você tem autorização para ocupar a vaga? (s/n): ")
-autorizado = r.lower() == 's'
+
+# tem autorização para ocupar a vaga?
+# print(c, list(vagas_class.vagas_autorizadas))
+autorizado = False
+if c in list(vagas_class.vagas_autorizadas): 
+    r = input("Você tem autorização para ocupar a vaga? (s/n): ")
+    autorizado = r.lower() == 's'
+
 
 
 # tentando ocupar a vaga
